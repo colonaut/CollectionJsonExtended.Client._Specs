99b2d536b68ea8c15fbcf7dbe5ef99182389e4ca
colonaut <colonaut@gmx.de>
2014-01-10 01:56:27 +0100

    Test on CollectionJsonAttribute and SingletonFactory

--- CollectionJsonResultSpecs.cs
+++ CollectionJsonResultSpecs.cs
@@ -1,10 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using System.Web.Mvc.Routing;
-using System.Web.WebPages;
 using CollectionJsonExtended.Client.Attributes;
 using CollectionJsonExtended.Core;
 using Machine.Specifications;
@@ -12,53 +10,82 @@ using Machine.Specifications;
 // ReSharper disable InconsistentNaming
 namespace CollectionJsonExtended.Client._Specs
 {
-    public class FakeEntityWithId
+    internal class FakeEntityWithIntId
     {
         public int Id { get; set; }
         public string SomeString { get; set; }
     }
 
-    public class FakeController : Controller
+    internal class FakeEntityWithStringId
     {
-        public CollectionJsonResult<FakeEntityWithId> GetMethodWithIntIdParam(int id)
-        {
-            return new CollectionJsonResult<FakeEntityWithId>(
-                new FakeEntityWithId
-                {
-                    Id = 1,
-                    SomeString = "Some string"
-                });
-        }
-        public CollectionJsonResult<FakeEntityWithId> GetMethodWithStringIdParam(string id)
-        {
-            return new CollectionJsonResult<FakeEntityWithId>(
-                new FakeEntityWithId
-                {
-                    Id = 1,
-                    SomeString = "Some string"
-                });
-        }
+        public string Id { get; set; }
+        public string SomeString { get; set; }
     }
 
-    public abstract class CollectionJsonAttributeContext
+    internal class FakeController : Controller
+    {
+        public CollectionJsonResult<FakeEntityWithIntId>
+            GetMethodWithIntIdParam(int id)
+        {
+            return new CollectionJsonResult<FakeEntityWithIntId>(
+                new FakeEntityWithIntId
+                {
+                    Id = 1,
+                    SomeString = "Some string"
+                });
+        }
+        
+        public CollectionJsonResult<FakeEntityWithStringId>
+            GetMethodWithStringIdParam(string id)
+        {
+            return new CollectionJsonResult<FakeEntityWithStringId>(
+                new FakeEntityWithStringId
+                {
+                    Id = "myStringId",
+                    SomeString = "Some string"
+                });
+        }
+        
+        public CollectionJsonResult<FakeEntityWithStringId>
+            GetMethodWithStringIdEntityButMethodInt(int id)
+        {
+            return new CollectionJsonResult<FakeEntityWithStringId>(
+                new FakeEntityWithStringId
+                {
+                    Id = "myStringId",
+                    SomeString = "Some string"
+                });
+        }
+        
+    }
+
+
+    internal abstract class CollectionJsonRouteAttributeContext
     {
         protected static ControllerDescriptor ControllerDescriptor;
         protected static ActionDescriptor[] GetMethodWithIntIdParamActionDescriptors;
         protected static ActionDescriptor[] GetMethodWithStringIdParamActionDescriptors;
+        protected static ActionDescriptor[] GetMethodWithStringIdParamButMethodIntActionDescriptors;
         protected static DirectRouteProviderContext DirectRouteProviderContext;
         protected static DirectRouteBuilder DirectRouteBuilder;
-        protected static CollectionJsonRouteAttribute CollectionJsonAttribute;
-
-        Establish context =
+        
+        protected static RouteInfo TheRouteInfo;
+        protected static RouteEntry TheRouteEntry;
+        protected static CollectionJsonRouteAttribute TheAttribute;
+        protected static IList<RouteInfo> TheUrlInfoCollection;
+        
+        Establish context  =
             () =>
             {
+
                 ControllerDescriptor =
-                    new ReflectedControllerDescriptor(typeof(FakeController));
+                    new ReflectedControllerDescriptor(typeof (FakeController));
 
                 GetMethodWithStringIdParamActionDescriptors =
                     new ActionDescriptor[]
                     {
-                        new ReflectedActionDescriptor(typeof (FakeController).GetMethod("GetMethodWithStringIdParam"),
+                        new ReflectedActionDescriptor(
+                            typeof (FakeController).GetMethod("GetMethodWithStringIdParam"),
                             "GetMethodWithStringIdParam",
                             ControllerDescriptor)
                     };
@@ -68,23 +95,67 @@ namespace CollectionJsonExtended.Client._Specs
                     {
                         new ReflectedActionDescriptor(typeof (FakeController).GetMethod("GetMethodWithIntIdParam"),
                             "GetMethodWithIntIdParam",
-                            ControllerDescriptor),                            
+                            ControllerDescriptor)
                     };
+
+                GetMethodWithStringIdParamButMethodIntActionDescriptors =
+                    new ActionDescriptor[]
+                    {
+                        new ReflectedActionDescriptor(typeof (FakeController)
+                            .GetMethod("GetMethodWithStringIdEntityButMethodInt"),
+                            "GetMethodWithStringIdParamButMethodInt",
+                            ControllerDescriptor)
+                    };
+            
                 
+
             };
+        
     }
 
 
-    [Subject(typeof(CollectionJsonRouteAttribute), "RouteInfo creation in CollectionJsonRouteAttribute instance")]
-    public class When_the_CollectionJsonAttribute_is_instanciated_wo_CreateRoute_but_fakes
-        : CollectionJsonAttributeContext
+    [Subject(typeof(CollectionJsonRouteAttribute),
+        "CollectionJsonAttribute.CreateRouteInfo")]
+    internal class When_the_Attribute_has_input_for_controller_string_method_but_int_entity
+        : CollectionJsonRouteAttributeContext
     {
-        //TODO do we want to get the token to replace from the route data constraints?
-        
         Establish context =
             () =>
             {
-                CollectionJsonAttribute =
+                const string template = "some/path/{id}";
+                TheAttribute =
+                    new CollectionJsonRouteAttribute(Is.Item, template);
+                DirectRouteBuilder =
+                    new DirectRouteBuilder(GetMethodWithStringIdParamButMethodIntActionDescriptors,
+                        true);
+                DirectRouteBuilder.Template = template;
+
+            };
+        
+        static Exception TheException;
+        Because of = () => TheException = Catch.Exception(
+            () =>
+            {
+                TheAttribute.CreateRouteInfo(DirectRouteBuilder);
+            });
+
+        It should_throw_a_ty = () => TheException.ShouldBeOfType(typeof(TypeAccessException));
+
+    }
+
+
+    [Subject(typeof(CollectionJsonRouteAttribute),
+        "CollectionJsonAttribute.CreateRoute")]
+    internal class When_the_Attribute_has_valid_input_for_controller_int_method
+        : CollectionJsonRouteAttributeContext
+    {
+        Establish context =
+            () =>
+            {
+                var singletonFactory =
+                    new SingletonFactory<UrlInfoCollection>(() => new UrlInfoCollection());
+                
+                TheAttribute =
                     new CollectionJsonRouteAttribute(Is.Item, "some/path/{myPrimaryKeyIsId}");
 
                 DirectRouteProviderContext =
@@ -92,44 +163,84 @@ namespace CollectionJsonExtended.Client._Specs
                         GetMethodWithIntIdParamActionDescriptors,
                         new DefaultInlineConstraintResolver(), true);
 
-                DirectRouteBuilder =
-                    DirectRouteProviderContext.CreateBuilder("some/path/{myPrimaryKeyIsId}");
+                TheRouteEntry =
+                    TheAttribute.CreateRoute(DirectRouteProviderContext);
 
-                    new DirectRouteBuilder(GetMethodWithIntIdParamActionDescriptors,
-                        true);
-                
+                TheUrlInfoCollection =
+                    singletonFactory.GetInstance()
+                        .Find<RouteInfo>(typeof (FakeEntityWithIntId))
+                        .ToList();
             };
-
-        //static IList<RouteInfo> TheUrlInfoCollection;
-        static RouteInfo TheRouteInfo;
 
         Because of =
             () =>
             {
-                //var routeData = attr.CreateRoute(DirectRouteProviderContext);
-                //TheUrlInfoCollection = Singleton<UrlInfoCollection>.Instance
-                //    .Find<RouteInfo>(typeof(FakeEntityWithId))
-                //    .ToList();
-
-                TheRouteInfo = CollectionJsonAttribute.CreateRouteInfo(DirectRouteBuilder);
+                TheRouteInfo =
+                    TheUrlInfoCollection[0];
+                //TheAttribute.CreateRouteInfo(DirectRouteBuilder);
             };
 
-        //Flicker because create route generates a new instance,
-        //with ncrunch every time you write in a test... bad bad bad
-        //we should find a solution... because if we call CreateRoute, the RouteInfo is also created multiple...
-        //It should_foo =
-        //    () => urlInfoCollection.Count.ShouldEqual(1);
-        //It should_foo1 =
-        //    () => urlInfoCollection[0].ShouldBeOfType<RouteInfo>();
+        private It should_foo =
+            () => TheUrlInfoCollection.Count.ShouldEqual(1);
 
-        It should_foo2 =
+        private It should_foo1 =
+            () => TheUrlInfoCollection[0].ShouldBeOfType<RouteInfo>();
+
+        private It should_foo2 =
             () => TheRouteInfo.Kind.ShouldEqual(Is.Item);
 
         private It shouuld_foo3 =
             () => TheRouteInfo.PrimaryKeyTemplate.ShouldEqual("{myPrimaryKeyIsId}");
-
-
     }
 
 
+    [Subject(typeof(CollectionJsonRouteAttribute),
+        "RouteInfo creation in CollectionJsonRouteAttribute instance (string emthod)")]
+    internal class When_the_Attribute_has_valid_input_for_controller_string_method
+        : CollectionJsonRouteAttributeContext
+    {
+        Establish context =
+            () =>
+            {
+                new SingletonFactory<UrlInfoCollection>(() => new UrlInfoCollection());
+
+                TheAttribute =
+                    new CollectionJsonRouteAttribute(Is.Item, "some/path/{myPrimaryKeyIsIdAndAString}");
+
+                DirectRouteProviderContext =
+                    new DirectRouteProviderContext("", "",
+                        GetMethodWithStringIdParamActionDescriptors,
+                        new DefaultInlineConstraintResolver(), true);
+
+                TheRouteEntry =
+                    TheAttribute.CreateRoute(DirectRouteProviderContext);
+
+                TheUrlInfoCollection =
+                    new SingletonFactory<UrlInfoCollection>().GetInstance()
+                        .Find<RouteInfo>(typeof(FakeEntityWithStringId))
+                        .ToList();
+            };
+
+        Because of =
+            () =>
+            {
+                TheRouteInfo =
+                    TheUrlInfoCollection[0];
+                //TheAttribute.CreateRouteInfo(DirectRouteBuilder);
+            };
+
+        It should_foo =
+            () => TheUrlInfoCollection.Count.ShouldEqual(1);
+
+        It should_foo1 =
+            () => TheUrlInfoCollection[0].ShouldBeOfType<RouteInfo>();
+
+        It should_foo2 =
+            () => TheRouteInfo.Kind.ShouldEqual(Is.Item);
+
+        It shouuld_foo3 =
+            () => TheRouteInfo.PrimaryKeyTemplate.ShouldEqual("{myPrimaryKeyIsIdAndAString}");
+
+
+    }
 }
